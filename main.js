919681f2930288d5d01da9b479451973471c45f9
Nuria <nurborgen@alu.edu.gva.es>
2023-09-29 18:28:03 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+import './style.css'
+import logoBatoi from '/logoBatoi.png'
+
+import {
+  booksFromUser,
+  booksFromModule,
+  booksCheeperThan,
+  booksWithStatus,
+  averagePriceOfBooks,
+  booksOfTypeNote,
+  booksNotOfTypeNote,
+  booksNotSold,
+  incrementPriceOfbooks,
+  getUserById,
+  getUserIndexById,
+  getUserByNickName,
+  getModuleByCode,
+  getModuleIndexByCode
+} from './src/functions'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${logoBatoi}" class="logo" alt="Vite logo" />
+    </a>
+    <h1>BatoiBooks</h1>
+    <p class="read-the-docs">
+      Abre la consola para ver el resultado
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
---
+++ src/functions.js
@@ -0,0 +1,16 @@
+export {
+    booksFromUser,
+    booksFromModule,
+    booksCheeperThan,
+    booksWithStatus,
+    averagePriceOfBooks,
+    booksOfTypeNote,
+    booksNotOfTypeNote,
+    booksNotSold,
+    incrementPriceOfbooks,
+    getUserById,
+    getUserIndexById,
+    getUserByNickName,
+    getModuleByCode,
+    getModuleIndexByCode
+  }
