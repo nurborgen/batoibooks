1da8ed75b1b9c29909a4ee1d7d720a50e0c07f76
Nuria <nurborgen@alu.edu.gva.es>
2023-10-04 15:38:34 +0200

    correcció en fallos de batoibooks

--- test/main.test.js
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from 'vitest'
-import data from '../datos.js'
-
-describe('Ficheros creados', () => {
-  test('Existe data en datos.js y contiene 6 libros', () => {
-    expect(data.books.length).toBe(6)
-  })
-})
